3f9094e9f617996ece21771f988dd763965b623c
atamshkai <66311425+atamshkai@users.noreply.github.com>
2024-08-23 15:10:32 +0630

    termux-desktop2.sh

--- termux-desktop2.sh
+++ termux-desktop2.sh
@@ -27,7 +27,7 @@ wget https://archive.org/download/github-atamshkai-termux-desktop2/win.tar.xz
 tar -xvJf win.tar.xz
 echo "termux-x11 :0 -xstartup 'dbus-launch --exit-with-session startxfce4' &>/dev/null" >>~/../usr/bin/tm-x11
 chmod +x ~/../usr/bin/tm-x11
-echo "pkill -f 'app_process / com.termux.x11'" >>~/../usr/bin/stop-tm-x11
+echo "pkill -f com.termux.x11" >>~/../usr/bin/stop-tm-x11
 chmod +x ~/../usr/bin/stop-tm-x11
 echo "Please Login Again"
 sleep 3
